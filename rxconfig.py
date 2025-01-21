238bdc251d26adfe090ac1d17933a984ce012a89
wgronholm <35667298+wgronholm@users.noreply.github.com>
2025-01-21 21:10:04 +0100

    Update cloudbuild

--- rxconfig.py
+++ rxconfig.py
@@ -7,8 +7,9 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://127.0.0.1:8000/")
 
 config = rx.Config(
     app_name="frontend", 
-    backend_port=8000, 
+    backend_port=8080, 
     frontend_port=8080, 
     api_url = BACKEND_URL,
     deploy_url = FRONTEND_URL,
+    cors_allowed_origins=["*"],    
 )
