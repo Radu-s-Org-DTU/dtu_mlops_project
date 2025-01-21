f2ccba2801d71b08b1f621565803365421b9ea36
wgronholm <35667298+wgronholm@users.noreply.github.com>
2025-01-21 19:20:14 +0100

    Update integration test

--- tests/integrationtests/test_apis.py
+++ tests/integrationtests/test_apis.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi.testclient import TestClient
 
 from src.mushroomclassification.api import app
@@ -6,7 +8,8 @@ client = TestClient(app)
 
 def test_predict_with_real_image():
     with TestClient(app) as client:
-        with open(r"data\raw_subset\Classes\edible\Agaricus_bisporus\Agaricus_bisporus29.png", "rb") as img_file:
+        file_path = os.path.join("data", "raw_subset", "Classes", "edible", "Agaricus_bisporus", "Agaricus_bisporus29.png")
+        with open(file_path, "rb") as img_file:
             response = client.post("/predict/", files={"file": ("image.jpg", img_file, "image/jpeg")})
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
