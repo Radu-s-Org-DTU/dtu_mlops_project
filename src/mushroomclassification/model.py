c8419cc34b66d8b858c78fd92ca053724278402d
Radu Grecu <radugrecu97@gmail.com>
2025-01-22 21:23:31 +0100

    Working W&B

--- src/mushroomclassification/model.py
+++ src/mushroomclassification/model.py
@@ -2,7 +2,8 @@ import lightning as L
 import torch
 from torch import nn, optim
 from torchmetrics import Accuracy
-
+import wandb
+from lightning.pytorch.loggers import WandbLogger
 
 class MushroomClassifier(L.LightningModule):
     """My awesome model."""
@@ -48,7 +49,8 @@ class MushroomClassifier(L.LightningModule):
         # Log training loss
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log("train_acc", acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-
+        wandb.log({"train_loss": loss.item()})
+        wandb.log({"train_acc": acc.item()})
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -61,12 +63,14 @@ class MushroomClassifier(L.LightningModule):
         acc = (target == preds.argmax(dim=-1)).float().mean()
         self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log('val_acc', acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        wandb.log({"val_loss": loss.item()})
+        wandb.log({"val_acc": acc.item()})
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         loss = self.criterion(logits, y)
-        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def on_save_checkpoint(self, checkpoint: dict) -> dict:
