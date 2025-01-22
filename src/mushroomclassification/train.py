09afaee9ad94ccb2d21ad478cf4c71c2cb7263c1
Radu Grecu <radugrecu97@gmail.com>
2025-01-22 21:23:31 +0100

    Working W&B

--- src/mushroomclassification/train.py
+++ src/mushroomclassification/train.py
@@ -1,5 +1,4 @@
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
-from lightning.fabric.utilities.seed import seed_everything
 from lightning.pytorch.cli import LightningCLI
 from model import MushroomClassifier
 from utils.config_loader import load_config
@@ -7,10 +6,27 @@ from utils.config_loader import load_config
 # from data import
 from data import MushroomDatamodule
 
+import wandb
+from dotenv import load_dotenv
+import os
+
 def train():
+    load_dotenv()
+    env = os.getenv
     config = load_config()
 
-    seed_everything(config['model']['seed'], workers=True)
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project=env("WANDB_PROJECT"),
+
+        # track hyperparameters and run metadata
+        config={
+            "learning_rate": config['trainer']['learning_rate'],
+            "architecture": "CNN",
+            "dataset": "Mushroom classification",
+            "epochs": config['trainer']['max_epochs'],
+        }
+    )
 
     checkpoint_callback = ModelCheckpoint(
         dirpath="models/",
