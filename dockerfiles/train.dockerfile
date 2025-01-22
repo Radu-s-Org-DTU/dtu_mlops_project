a1a2a1a3fbec67fe3db5b92855833bc3b0013774
wgronholm <35667298+wgronholm@users.noreply.github.com>
2025-01-22 16:09:25 +0100

    Update dockerfile

--- dockerfiles/train.dockerfile
+++ dockerfiles/train.dockerfile
@@ -4,11 +4,6 @@ RUN apt update && \
     apt install --no-install-recommends -y build-essential gcc curl ca-certificates && \
     apt clean && rm -rf /var/lib/apt/lists/*
 
-RUN curl -sSL https://sdk.cloud.google.com | bash -s -- --disable-prompts && \
-    echo 'export PATH=$PATH:/root/google-cloud-sdk/bin' >> /etc/profile.d/google-cloud-sdk.sh && \
-    export PATH=$PATH:/root/google-cloud-sdk/bin && \
-    gcloud --version
-
 RUN pip install google-cloud-storage
 
 COPY requirements.txt requirements.txt
@@ -29,4 +24,4 @@ RUN mkdir -p /app
 COPY pull_data.sh /app/pull_data.sh
 RUN chmod +x /app/pull_data.sh
 
-ENTRYPOINT ["/bin/bash", "-c", "/app/pull_data.sh && invoke train"]
+ENTRYPOINT ["sh", "-c", "python src/mushroomclassification/pull_data.py && invoke train"]
---
+++ src/mushroomclassification/pull_data.py
@@ -0,0 +1,23 @@
+from google.cloud import storage
+import os
+
+def download_from_gcs(bucket_name, source_path, destination_path):
+    os.makedirs(destination_path, exist_ok=True)
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=source_path)
+
+    for blob in blobs:
+        dest_file_path = os.path.join(destination_path, blob.name.replace(source_path, "").lstrip("/"))
+        os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
+        blob.download_to_filename(dest_file_path)
+
+if __name__ == "__main__":
+    BUCKET_NAME = "02476-data"
+    RAW_PATH = "data/raw"
+    RAW_SUBSET_PATH = "data/raw_subset"
+
+    download_from_gcs(BUCKET_NAME, RAW_PATH, "/app/data/raw")
+    download_from_gcs(BUCKET_NAME, RAW_SUBSET_PATH, "/app/data/raw_subset")
+
+    print("Data pulled successfully.")
