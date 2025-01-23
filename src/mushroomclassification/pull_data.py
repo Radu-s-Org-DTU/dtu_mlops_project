0fb530c45923a246343e4ea5a44f48379a6a1154
wgronholm <35667298+wgronholm@users.noreply.github.com>
2025-01-23 10:45:11 +0100

    Update pull_data.py

--- src/mushroomclassification/pull_data.py
+++ src/mushroomclassification/pull_data.py
@@ -1,6 +1,7 @@
 import os
 
 from google.cloud import storage
+from utils.config_loader import load_config
 
 
 def download_from_gcs(bucket_name, source_path, destination_path):
@@ -15,11 +16,14 @@ def download_from_gcs(bucket_name, source_path, destination_path):
         blob.download_to_filename(dest_file_path)
 
 if __name__ == "__main__":
-    BUCKET_NAME = "02476-data"
-    RAW_PATH = "data/raw"
-    RAW_SUBSET_PATH = "data/raw_subset"
+    path = load_config()['data']['data_path']
+    available_paths = ["data/raw", "data/raw_subset"]
 
-    #download_from_gcs(BUCKET_NAME, RAW_PATH, "/app/data/raw")
-    download_from_gcs(BUCKET_NAME, RAW_SUBSET_PATH, "/app/data/raw_subset")
+    if path not in available_paths:
+        raise ValueError(f"Invalid data path: {path}. Available paths: {available_paths}")
+
+    BUCKET_NAME = "02476-data"
+
+    download_from_gcs(BUCKET_NAME, path, '/app/' + path)
 
     print("Data pulled successfully.")
